a026281c23cd5945246bb1d830de0839184e1c96
ilia <jamiashvili@gmail.com>
2020-07-28 08:41:21 +0600

    Added partial template task_form.html and created forms.py with a TaskForm class

---
+++ source/webapp/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from django.forms import widgets
+from webapp.models import STATUS_CHOICES
+
+
+class TaskForm(forms.Form):
+    description = forms.CharField(max_length=200, required=True, label='Description')
+    detailed_desc = forms.CharField(max_length=1500, required=False, label='Detailed description',
+                                    widget=widgets.Textarea)
+    status = forms.ChoiceField(required=True, label='Status', choices=STATUS_CHOICES)
+    finish_date = forms.DateField(required=False, widget=forms.DateInput(attrs={'placeholder': 'YYYY-MM-DD'}))
