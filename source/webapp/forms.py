0e633425e1ba3dce09ede71432646defff3189f0
ilia <jamiashvili@gmail.com>
2020-07-29 11:01:08 +0600

    Added initial status choice in forms.py, added 'Cancel' button in admin_delete.html

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -7,6 +7,6 @@ class TaskForm(forms.Form):
     description = forms.CharField(max_length=200, required=True, label='Description')
     detailed_desc = forms.CharField(max_length=1500, required=False, label='Detailed description',
                                     widget=widgets.Textarea)
-    status = forms.ChoiceField(required=True, label='Status', choices=STATUS_CHOICES)
+    status = forms.ChoiceField(required=True, label='Status', choices=STATUS_CHOICES, initial=STATUS_CHOICES[0][0])
     finish_date = forms.DateField(required=False, label='Finish date', widget=forms.DateInput(attrs={
         'placeholder': 'YYYY-MM-DD'}))
