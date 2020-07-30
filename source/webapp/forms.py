311da4960f2552aac4cf949f7f38e92e9d267849
ilia <jamiashvili@gmail.com>
2020-07-30 08:01:36 +0600

    Moved 'placeholder' attr from the form widget attributes in forms.py to task_form.html, performed minor text changes in index.html and admin_delete.html

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -8,5 +8,4 @@ class TaskForm(forms.Form):
     detailed_desc = forms.CharField(max_length=1500, required=False, label='Detailed description',
                                     widget=widgets.Textarea)
     status = forms.ChoiceField(required=True, label='Status', choices=STATUS_CHOICES, initial=STATUS_CHOICES[0][0])
-    finish_date = forms.DateField(required=False, label='Finish date', widget=forms.DateInput(attrs={
-        'placeholder': 'YYYY-MM-DD'}))
+    finish_date = forms.DateField(required=False, label='Finish date', widget=forms.DateInput)
